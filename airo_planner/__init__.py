7b3122e7858efb6f9220b18392f1fc9bb8a813a7
victorlouisdg <victorlouisdg@gmail.com>
2024-03-04 17:33:53 +0100

    prevent isort messing up order in __init__.py

--- airo_planner/__init__.py
+++ airo_planner/__init__.py
@@ -3,15 +3,16 @@
 # but for building and many toolings, you still need to have __init__ files (at least in the root of the package).
 # e.g. if you remove this init file and try to build with pip install .
 # you won't be able to import the dummy module.
-from airo_planner.interfaces import DualArmPlanner, SingleArmPlanner
-from airo_planner.ompl.dual_arm_ompl_planner import DualArmOmplPlanner
-from airo_planner.ompl.single_arm_ompl_planner import InverseKinematicsType, SingleArmOmplPlanner
-from airo_planner.ompl.state_space import (
+from airo_planner.ompl.state_space import (  # isort:skip
     function_numpy_to_ompl,
     numpy_to_ompl_state,
     ompl_path_to_numpy,
     ompl_state_to_numpy,
 )
+
+from airo_planner.interfaces import DualArmPlanner, SingleArmPlanner  # isort:skip
+from airo_planner.ompl.single_arm_ompl_planner import InverseKinematicsType, SingleArmOmplPlanner  # isort:skip
+from airo_planner.ompl.dual_arm_ompl_planner import DualArmOmplPlanner  # isort:skip
 
 __all__ = [
     "SingleArmPlanner",
